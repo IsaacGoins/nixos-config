7c4e4285a56b265011d8bbaca0075c15eba614fc
Isaac Goins <igoins200@gmail.com>
2025-08-27 00:20:18 -0500

    Updated vm config

--- modules/programs/desktop-programs.nix
+++ modules/programs/desktop-programs.nix
@@ -3,11 +3,11 @@
   # Enable Steam
   programs.steam.enable = true;
 
-  # Enable Virt-manager
-  programs.virt-manager.enable = true;
-  users.groups.libvirtd.members = [ "igoins" ];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
+# Enable Virt-manager
+programs.virt-manager.enable = true;
+users.groups.libvirtd.members = ["igoins"];
+virtualisation.libvirtd.enable = true;
+virtualisation.spiceUSBRedirection.enable = true;
 
   environment.systemPackages = with pkgs; [
     discord
@@ -24,7 +24,6 @@
     kdePackages.plasma-systemmonitor
     kdePackages.konsole
     kdePackages.partitionmanager
-    qemu
-    quickemu
+		qemu
   ];
 }
