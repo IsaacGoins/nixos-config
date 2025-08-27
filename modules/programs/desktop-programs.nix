f5e0b88e9eb7fb487152f5f6a9159fd7069e8419
Isaac Goins <igoins200@gmail.com>
2025-08-26 23:55:02 -0500

    Added QEMU and Virt-manager

--- modules/programs/desktop-programs.nix
+++ modules/programs/desktop-programs.nix
@@ -3,6 +3,12 @@
   # Enable Steam
   programs.steam.enable = true;
 
+  # Enable Virt-manager
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = [ "igoins" ];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+
   environment.systemPackages = with pkgs; [
     discord
     orca-slicer
