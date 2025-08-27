351fca42c8d40910f7c8a5513ad200e9a559975b
Isaac <igoins200@gmail.com>
2025-08-27 12:05:46 -0500

    Fixed bluetooth policy issue

--- modules/nixos/basic.nix
+++ modules/nixos/basic.nix
@@ -57,12 +57,12 @@
         # the tradeoff is increased power consumption. Defaults to
         # 'false'.
         FastConnectable = true;
-        Policy = {
-          # Enable all controllers when they are found. This includes
-          # adapters present on start as well as adapters that are plugged
-          # in later on. Defaults to 'true'.
-          AutoEnable = true;
-        };
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
       };
     };
   };
